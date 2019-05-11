2018a08aca98f45683b14a588e6085cff2154b3d
Paul Tobin <tobster111@gmail.com>
2019-05-11 10:30:12 +0100

    Added two buttons to the App component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <button>Posts</button>
+          <button>Todos</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
