1624f5e848c5547a0f54d3ac6c8255187d04e75a
Paul Tobin <tobster111@gmail.com>
2019-05-11 13:34:23 +0100

    Using material-ui for styling the component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
 import React, { useState } from 'react';
 
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 const App = () => {
   const [resource, setResource] = useState('posts');
   console.log(resource);
   return (
     <div>
       <div>
-        <button onClick={() => setResource('posts')}>Posts</button>
-        <button onClick={() => setResource('todos')}>Todos</button>
+        <Button
+          variant="outlined"
+          color="default"
+          onClick={() => setResource('posts')}
+        >
+          Posts
+        </Button>
+        <Button
+          variant="outlined"
+          color="default"
+          onClick={() => setResource('todos')}
+        >
+          Todos
+        </Button>
       </div>
-      {resource}
+      <TextField
+        id="standard-bare"
+        className={resource}
+        margin="normal"
+        value={resource}
+      >
+        {resource}
+      </TextField>
     </div>
   );
 };
