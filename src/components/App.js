b36b76974327a6596cc1d0116cd26c5002ec76c0
Paul Tobin <tobster111@gmail.com>
2019-05-11 13:54:47 +0100

    Created ResourceList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 
+import ResourceList from './ResourceList';
+
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
 const App = () => {
   const [resource, setResource] = useState('posts');
+
   console.log(resource);
   return (
     <div>
@@ -29,9 +32,8 @@ const App = () => {
         className={resource}
         margin="normal"
         value={resource}
-      >
-        {resource}
-      </TextField>
+      />
+      <ResourceList resource={resource}/>
     </div>
   );
 };
---
+++ src/components/ResourceList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ResourceList = (props) => {
+  return (
+    <div>
+      <h1>list</h1>
+      {props.resource}
+    </div>
+  )
+}
+
+export default ResourceList
