a1e61152608d2411c4c24bb55761a9d75cdbc587
Paul Tobin <tobster111@gmail.com>
2019-05-12 11:19:58 +0100

    Added a second arguement to useEffect to call the function when resource is changed

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const getData = async () => {
-    const res = await fetch(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
-    );
+  const getData = async resource => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`);
     const data = await res.json();
-    return setResources({ resources: data });
+    return setResources(data);
   };
+
+  useEffect(() => {
+    getData(resource);
+  }, [resource]);
 
   return (
     <div>
